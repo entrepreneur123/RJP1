aff9d02e145a058637561888be30ec7c75612267
@entrepreneur123 <irusha2056@gmail.com>
2021-04-02 11:08:45 +0545

    RJP1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const firstName= 'John';
+  const lastName = 'WICk';
+  const age = 28;
+  const job = 'Gentleman';
+
+  const inputPlaceholder = 'Enter your details'
+
+  const getFuLLName = (firstName,lastName) => `${firstName} ${lastName}`
+  const mArr = [1,2,3,4]
+
+  const detailsInputBox = <input placeholder ={inputPlaceholder} autocomplete />;
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Full Name: {getFuLLName(firstName, lastName)}</h3>
+      <p>Age: {age}</p>
+      <p>Job: {job}</p>
+      <imput placeholder= {inputPlaceholder}/>
+      {mArr[0]}
+      {
+        mArr[0] > 0 ? "True" :"False"      }
+      {detailsInputBox}
+      
     </div>
   );
 }
